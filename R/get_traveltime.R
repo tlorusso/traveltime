3df8b237935b0b162845ead7133525b9acdb42e4
tlo <tlorusso@users.noreply.github.com>
2018-07-19 20:46:05 +0200

    as.spatial option added

--- R/get_traveltime.R
+++ R/get_traveltime.R
@@ -8,6 +8,7 @@
 #' @traveltime traveltime in seconds
 #' @type transportation mode
 #' @departure time of departure, format: "2018-08-05T08:00:00Z"
+#' @as.spatial defaults to FALSE, if set to TRUE the resulting object is of class "spatial" instead of sf.
 #' @importFrom jsonlite fromJSON
 #' @importFrom httr POST
 #' @importFrom sf st_as_sf
@@ -23,7 +24,7 @@
 #' plot(poly)
 #' }
 
-get_traveltime<- function(...){
+get_traveltime<- function(..., as.spatial=FALSE){
 
 traveltimelist <-traveltime_request(...)
 
@@ -34,7 +35,17 @@ ttlist2 <-traveltimelist %>%
 listnew <- ttlist2%>%
   map(~make_polygon(.))
 
+
+
+if(as.spatial==FALSE){
+
 do.call(rbind,listnew)
+
+}else{
+
+do.call(rbind,listnew) %>%
+    as_Spatial()
+}
 
 
 }
