2a573a8915d6608a1e3d886f11122cc77ab97892
tlo <tlorusso@users.noreply.github.com>
2018-07-19 20:41:59 +0200

    documentation created

--- R/get_traveltime.R
+++ R/get_traveltime.R
@@ -5,8 +5,9 @@
 #' @param appId Traveltime-Plattform Application ID
 #' @param apiKey Traveltime-Plattform API-Key
 #' @param location vector of lat / long coordinates, example: c(47.233,8.234)
-#' @driveTime traveltime (not implemented yet)
-#' @transtype transportation mode (not implemented yet)
+#' @traveltime traveltime in seconds
+#' @type transportation mode
+#' @departure time of departure, format: "2018-08-05T08:00:00Z"
 #' @importFrom jsonlite fromJSON
 #' @importFrom httr POST
 #' @importFrom sf st_as_sf
@@ -15,9 +16,10 @@
 #' @rdname get_traveltime
 #'
 #' @return the results from the search
+#' @family get_traveltime functions
 #' @examples
 #' \dontrun{
-#' req <- traveltime_request(appId, apiKey,location,driveTime)
+#' req <- traveltime_request(appId, apiKey,location=c(47.378610,8.54000),traveltime=1800,departure="2018-08-05T08:00:00Z")
 #' plot(poly)
 #' }
 
--- R/traveltime_request.R
+++ R/traveltime_request.R
@@ -1,18 +1,16 @@
-traveltime_request <- function(appId, apiKey, location, driveTime){
-  
-  
-  
+traveltime_request <- function(appId, apiKey, location, traveltime, type="public_transport", departure){
+
   url <- "http://api.traveltimeapp.com/v4/time-map"
-  
-  
-  requestBody <- paste0('{ 
-                        "departure_searches" : [ 
-                        {"id" : "Zurich", 
-                        "coords": {"lat":', location[1], ', "lng":', location[2],' }, 
-                        "transportation" : {"type" : "public_transport"} ,
-                        "travel_time" : ', driveTime, ',
-                        "departure_time" : "2018-08-05T08:00:00Z"
-                        } 
+
+
+  requestBody <- paste0('{
+                        "departure_searches" : [
+                        {"id" : "request",
+                        "coords": {"lat":', location[1], ', "lng":', location[2],' },
+                        "transportation" : {"type" : "',type,'"} ,
+                        "travel_time" : ', traveltime, ',
+                        "departure_time" : "', departure,'"
+                        }
                         ] }')
 
   response <- httr::POST(url = url,
@@ -22,21 +20,21 @@ traveltime_request <- function(appId, apiKey, location, driveTime){
                      httr::add_headers('X-Api-Key' = apiKey),
                      body = requestBody,
                      encode = "json")
-  
-  
-  res2 <- jsonlite::fromJSON(as.character(response))  
-  
+
+
+  res2 <- jsonlite::fromJSON(as.character(response))
+
   # the shapes list contains all the polygons
-  flat <- res2$results$shapes[[1]]$shell %>% 
+  flat <- res2$results$shapes[[1]]$shell %>%
     purrr::map_dfr(bind_cols,.id="group")
-  
+
    #keine names, daher suboptima
   # do.call(rbind,res2$results$shapes[[1]]$shell)
   # Reduce(rbind, res2$results$shapes[[1]]$shell)
-  
-  sf::st_as_sf(x = flat, 
+
+  sf::st_as_sf(x = flat,
                       coords = c("lng", "lat"),
                       crs = "+proj=longlat +datum=WGS84")
-  
-  
+
+
 }
