df764e1ac80a1660805e1a83561fc513bd04ba37
tlo <tlorusso@users.noreply.github.com>
2018-09-17 22:20:54 +0200

    fixed make polygons function

--- R/make_polygons.R
+++ R/make_polygons.R
@@ -1,14 +1,11 @@
 make_polygons <- function(sf_points){
 
-
-sfp2 <- st_sfc(st_geometry(sf_points)) %>%
-    st_cast("MULTIPOINT", ids = "group") %>%
-    st_cast("LINESTRING",ids="group")
-
-  st_sf(sfp2) %>%
-    # mutate(geometry=sfp2) %>%
-    mutate(id=sf_points$group[1])
-
+sfp %>%
+    mutate(ID=row_number()) %>%
+    group_by(group) %>%
+    arrange(ID) %>%
+    summarize(INT = first(ID), do_union = FALSE) %>%
+    st_cast("POLYGON")
 
 }
 
