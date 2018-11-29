83ae0b0d76e1ea833d62121f1db23116321b38c5
tlo <tlorusso@users.noreply.github.com>
2018-11-29 13:39:21 +0100

    revert to old stage

--- R/get_traveltime.R
+++ R/get_traveltime.R
@@ -48,7 +48,6 @@ polygonslist <- splitlist %>%
   purrr::map(~make_polygons(.))
 
 do.call(rbind, polygonslist) %>%
-  st_combine() %>%
-  st_sf(traveltime=traveltime, geometry = .)
+  dplyr::mutate(traveltime = traveltime)
 
 }
