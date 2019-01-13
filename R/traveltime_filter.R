ed64186e55a3f13fe40665e87fd7716ce74b74eb
tlo <tlorusso@users.noreply.github.com>
2019-01-13 14:34:41 +0100

    documentation modified

--- R/traveltime_filter.R
+++ R/traveltime_filter.R
@@ -1,12 +1,49 @@
-#' #' API Wrapper for the Traveltime API
-#' #'
-#' #' traveltime_filter calls the traveltime-filter API module to calculate traveltime between coordinates
-#' #'
-#' #' @importFrom httr POST
-#' #' @noRd
+#' Calculate traveltime between locations
+#'
+#' \code{traveltime_filter} is the main function of the traveltimeR package. It retrieves traveltime-isochrones from the traveltime platform, which supports several modes of transport in several countries and brings them into r as polygons stored in sf-objects.
+#'
+#'   traveltime_filter - retrieve isochrones for traveltimemaps from Traveltime API
+#'
+#' @param appId Traveltime-Plattform Application ID
+#' @param apiKey Traveltime-Plattform API-Key
+#' @param from vector of lat / long coordinates, coordinate system WGS84 / espg 4326, example: c(47.233,8.234).
+#' @param to vector or list of lat / long coordinates, coordinate system WGS84 / espg 4326, example: c(47.233,8.234).
+#' @param traveltime traveltime in seconds
+#' @param type transportation mode
+#' @param departure time of departure, date in extended ISO-8601 format (incl. timezone), example: "2018-08-05T08:00:00Z"
+#' @param arrival time of arrival, date in extended ISO-8601 format (incl. timezone), example: "2018-08-05T08:00:00Z"
+#' @importFrom sf st_as_sf
+#' @importFrom purrr map_dfr
+#' @importFrom dplyr bind_cols
+#' @importFrom dplyr "%>%"
+#' @importFrom dplyr mutate
+#' @export
+#' @rdname traveltime_filter
+#' @details To call the API via the package you need to get an api-key here \url{http://docs.traveltimeplatform.com/overview/getting-keys/}.
+#'          If you're not using the API for commercial use users can get 10,000 free API queries a month. The following transport modes are supported (type):"cycling"", "cycling_ferry", "driving", "driving+train", "driving_ferry", "public_transport", "walking", "walking+coach", "walking_bus", "walking_ferry" or "walking_train".
+#' @return the results from the search
+#' @examples
+#'  \donttest{
+#'  #list of destinations
+#'locations <-list(c(47.378610,8.54000),c(47.578620,8.54000))
+#'
+#' traveltimes <- traveltime_filter(appId="YourAppId",
+#'                             apiKey="YourApiKey",
+#'                             from = c(47.578610,8.54000),
+#'                             to = locations ,
+#'                             traveltime=2000,
+#                              type="public_transport",
+#'                             departure="2018-12-06T08:00:00Z")
+#'
+#' #plot the isochrones
+#' glimpse(traveltimes)
+#' }
+#'
+#' @references \href{http://docs.traveltimeplatform.com/overview/introduction}{Traveltime Plattform API Docs}
+#' @export
 #'
 traveltime_filter <- function(appId = "yourAppId", apiKey = "yourApiKey", from = NULL, to = NULL , traveltime = 14400, type = NULL, departure = NULL){
-#
+
 #   #checks : missing parameters?
   if (length(to)<2) stop("vector of longitude / latitude coordinates missing", call. = FALSE)
 #   if (is.null(traveltime)) stop("traveltime missing - set traveltime (in seconds)", call. = FALSE)
