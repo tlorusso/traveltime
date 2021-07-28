58e7e52c973e5a19577ab8d69cf8e36c65580b87
Arthur Gailes <agailes1@gmail.com>
2021-07-28 13:31:27 -0400

    complete first-pass geocode function

--- R/traveltime_geocode.R
+++ R/traveltime_geocode.R
@@ -1,12 +1,15 @@
 #' Geocode addresses using the traveltime API
 #'
-#' TODO: enable vectors of addresses to be passed through directly.
+#' TODO: enable vectors of addresses to be passed through directly, along with time limit
 #' TODO: enable reverse geocoding
+#' TODO: refine time limit
+#'
 #' @importFrom httr GET content
 #' @importFrom sf st_read
 #'
 #' @param address A character vector of length one containing the address query.
 #'
+#' @inheritParams
 #' @examples
 #' \dontrun{
 #' whiteHouse <- c("1600 Pennsylvania Avenue NW, Washington, DC, 20500, United States", "The White House")
@@ -20,6 +23,12 @@
 #'
 #' }
 #' @export
-traveltime_geocode <- function(address){
+traveltime_geocode <- function(appId = "yourAppId", apiKey = "yourApiKey", address){
+  #req one row
+  req <- httr::GET(url = "http://api.traveltimeapp.com/v4/geocoding/search",
+      httr::add_headers("X-Application-Id"= tt_appid, "X-Api-Key"= tt_appkey),
+    query = list(query=address, within_country="United States"))
+  # return the json response as text
+  return(httr::content(req))
 
 }
